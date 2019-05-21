2068122d2d791bfa0f21ae83e7d9ca84aceda6eb
Alejandro <ale_arc2015@hotmail.com>
2019-05-20 22:38:08 -0300

    Agregar archivos de proyecto.

---
+++ Tp7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp7
+{
+    class Program
+    {
+        public struct Datos
+        {
+            public string Nombre;
+            public string Apellido;
+            public int DiaNac;
+            public int MesNac;
+            public int AñoNac;
+            public bool EstadoCivil;
+            public bool Género;
+            public int DiaIngreso;
+            public int MesIngreso;
+            public int AñoIngreso;
+            public double SueldoBasico;
+            public string Cargo;
+        }
+        public class Trabajo
+        {
+            enum Cargos {Auxiliar, Administrativo, Ingeniero, Especialista, Investigador}
+        }
+        static void Main(string[] args)
+        {
+            List<Datos> ListaDeEmpleados = new List<Datos>();
+        }
+    }
+}
